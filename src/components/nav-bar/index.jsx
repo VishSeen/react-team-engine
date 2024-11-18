44dab062bf3e20e0549b843c185190282ee430ef
VishSeen <vishseenarain@gmail.com>
2024-11-18 09:37:34 +0400

    [DEV] - pushing to git for use in work mac.

--- src/components/nav-bar/index.jsx
+++ src/components/nav-bar/index.jsx
@@ -4,19 +4,19 @@ const NavBar = ({
   navItems
 }) => {
   return (
-    <div className="nav-bar">
-      <ul style={{
+    <nav className="nav-bar" role="navigation" aria-label="main navigation">
+      <ul className="navbar-menu" style={{
         width: '100%'
       }}>
         {navItems.map(item => {
           return (
-            <li key={item.id}>
+            <li key={item.id} className='nav-item'>
               <a href={item.href}>{item.name}</a>
             </li>
           )
         })}
       </ul>
-    </div>
+    </nav>
   )
 }
 
